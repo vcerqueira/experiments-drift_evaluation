8c89ebb7508f8c7ffbb46bf93cae2f01d4c49758
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2024-11-05 22:18:27 +0000

    basic pipeline draft

--- scripts/drift_detection.py
+++ scripts/drift_detection.py
@@ -1,34 +1,22 @@
 from pprint import pprint
-import numpy as np
-import pandas as pd
-
-import capymoa.drift.detectors as detectors
-
-from capymoa.classifier import OnlineBagging
 
 from utils.streams import stream_sea2drift
-
+from capymoa.drift.detectors import ADWIN
+from capymoa.classifier import OnlineBagging
 from capymoa.evaluation.evaluation import ClassificationEvaluator
 
-##
 METRIC = 'accuracy'
 MIN_TRAINING_SIZE = 1000
 MAX_SIZE = 15000
 
 sch = stream_sea2drift.get_schema()
-
-# stream_sea2drift.get_schema()
-# stream_sea2drift.next_instance()
+detector = ADWIN(delta=0.001)
 
 evaluator = ClassificationEvaluator(schema=sch, window_size=1)
-
 learner = OnlineBagging(schema=sch, ensemble_size=10)
 
-# https://github.com/adaptive-machine-learning/CapyMOA/blob/main/src/capymoa/evaluation/evaluation.py#L888
-
-
-error_rate = []
 instances_processed = 0
+drift_predictions = []
 while stream_sea2drift.has_more_instances():
     if instances_processed > MAX_SIZE:
         break
@@ -40,11 +28,18 @@ while stream_sea2drift.has_more_instances():
 
         evaluator.update(instance.y_index, prediction)
 
+        score = evaluator.accuracy()
+
+        detector.add_element(score)
+        if detector.detected_change():
+            print(f'Change detected at index: {instances_processed}')
+            drift_predictions.append(instances_processed)
+
     learner.train(instance)
 
     instances_processed += 1
 
-evaluator.metrics_per_window()
-evaluator.accuracy()
+# evaluator.metrics_per_window()
+# evaluator.accuracy()
 
-pprint(evaluator.metrics_dict())
+# pprint(evaluator.metrics_dict())
