be3c5b67571c777f8a3f388536007a6f57df8443
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2024-11-07 22:06:45 +0000

    pipeline

--- scripts/drift_detection.py
+++ scripts/drift_detection.py
@@ -1,45 +1,74 @@
+from typing import Optional
 from pprint import pprint
 
 from utils.streams import stream_sea2drift
-from capymoa.drift.detectors import ADWIN
+from capymoa.drift.detectors import ADWIN, PageHinkley
 from capymoa.classifier import OnlineBagging
 from capymoa.evaluation.evaluation import ClassificationEvaluator
 
-METRIC = 'accuracy'
-MIN_TRAINING_SIZE = 1000
 MAX_SIZE = 15000
 
 sch = stream_sea2drift.get_schema()
-detector = ADWIN(delta=0.001)
-
 evaluator = ClassificationEvaluator(schema=sch, window_size=1)
 learner = OnlineBagging(schema=sch, ensemble_size=10)
 
-instances_processed = 0
-drift_predictions = []
-while stream_sea2drift.has_more_instances():
-    if instances_processed > MAX_SIZE:
-        break
 
-    instance = stream_sea2drift.next_instance()
+class StreamingWorkflow:
+    """
+    todo add adaptation mechanism
+        retraining on buffer?
 
-    if instances_processed > MIN_TRAINING_SIZE:
-        prediction = learner.predict(instance)
+    """
+    METRIC = 'accuracy'
+    MIN_TRAINING_SIZE = 1000
 
-        evaluator.update(instance.y_index, prediction)
+    def __init__(self, model, evaluator, detector):
+        self.model = model
+        self.evaluator = evaluator
+        self.detector = detector
+        self.instances_processed = 0
+        self.drift_predictions = []
 
-        score = evaluator.accuracy()
+    def run_prequential(self, stream, max_size: Optional[int] = None):
+        self._reset_params()
 
-        detector.add_element(score)
-        if detector.detected_change():
-            print(f'Change detected at index: {instances_processed}')
-            drift_predictions.append(instances_processed)
+        while stream.has_more_instances():
+            if self.instances_processed > max_size:
+                break
 
-    learner.train(instance)
+            instance = stream.next_instance()
+            if self.instances_processed > self.MIN_TRAINING_SIZE:
+                prediction = self.model.predict(instance)
 
-    instances_processed += 1
+                self.evaluator.update(instance.y_index, prediction)
 
-# evaluator.metrics_per_window()
-# evaluator.accuracy()
+                score = self._get_latest_score()
 
-# pprint(evaluator.metrics_dict())
+                self._update_detector(score)
+
+            self.model.train(instance)
+
+            self.instances_processed += 1
+
+    def _get_latest_score(self):
+        return self.evaluator.accuracy()
+
+    def _update_detector(self, score):
+        self.detector.add_element(score)
+        if self.detector.detected_change():
+            print(f'Change detected at index: {self.instances_processed}')
+            self.drift_predictions.append(self.instances_processed)
+
+    def _reset_params(self):
+        self.instances_processed = 0
+        self.drift_predictions = []
+
+
+# detector = ADWIN(delta=0.001)
+detector = PageHinkley()
+
+wf = StreamingWorkflow(model=learner,
+                       evaluator=evaluator,
+                       detector=detector)
+
+wf.run_prequential(stream=stream_sea2drift, max_size=MAX_SIZE)
