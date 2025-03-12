2d9b467ede9922816e6721b3fdb384601c57c236
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-03-12 11:28:31 +0000

    analysis

--- scripts/experiments/analysis/detector_evaluation.py
+++ scripts/experiments/analysis/detector_evaluation.py
@@ -1,36 +1,68 @@
 import pandas as pd
 
-mode = 'abrupt'
 
-# point = pd.read_csv(f'assets/{mode},OnlineBagging,point.csv').set_index('Unnamed: 0')
-# window = pd.read_csv(f'assets/{mode},ARF,window.csv').set_index('Unnamed: 0')
-# df = pd.read_csv(f'assets/results/STAGGER,ABRUPT,ARF,POINT.csv').set_index('Unnamed: 0')
-# df = pd.read_csv(f'assets/results/SEA,ABRUPT,ARF,POINT.csv').set_index('Unnamed: 0')
-# df = pd.read_csv(f'assets/results/Agrawal,ABRUPT,ARF,POINT.csv').set_index('Unnamed: 0')
-# df = pd.read_csv(f'assets/results/Electricity,ABRUPT@X,HoeffdingTree,POINT.csv').set_index('Unnamed: 0')
-df = pd.read_csv(f'assets/results/Electricity,ABRUPT@Y,HoeffdingTree.csv').set_index('Unnamed: 0')
-df = pd.read_csv(f'assets/results/Covtype,ABRUPT@Y,HoeffdingTree.csv').set_index('Unnamed: 0')
-df = pd.read_csv(f'assets/results/Electricity,ABRUPT@X,HoeffdingTree.csv').set_index('Unnamed: 0')
-df = pd.read_csv(f'assets/results/Covtype,ABRUPT@X,HoeffdingTree.csv').set_index('Unnamed: 0')
-df = pd.read_csv(f'assets/results/Electricity,ABRUPT@XY,HoeffdingTree.csv').set_index('Unnamed: 0')
-df = pd.read_csv(f'assets/results/Covtype,ABRUPT@XY,HoeffdingTree.csv').set_index('Unnamed: 0')
+def read_real(dataset: str, type: str, learner: str, metric: str):
+    df = pd.read_csv(f'assets/results/{dataset},{type},{learner}.csv').set_index('Unnamed: 0')
+    df.index.name = 'Detector'
 
-# point['f1']
-# window['f1']
+    df_metric = df[metric]
 
-# df = pd.concat([point['f1'], window['f1']], axis=1)
-# df = window
-# df.columns = ['point', 'window']
-print(df)
-print(df['f1'].sort_values())
-print(df['n_alarms'].sort_values())
+    return df_metric
 
-results = {}
-for stream in ['Agrawal', 'STAGGER', 'SEA']:
-    results[stream] = pd.read_csv(f'assets/results/{stream},ABRUPT,ARF,POINT.csv').set_index('Unnamed: 0')['f1']
 
-df = pd.DataFrame(results)
-df.mean(axis=1).sort_values()
+df_y = read_real('Electricity', 'ABRUPT@Y', 'HoeffdingTree', 'f1')
+df_x = read_real('Electricity', 'ABRUPT@X', 'HoeffdingTree', 'f1')
+df_xy = read_real('Electricity', 'ABRUPT@X', 'HoeffdingTree', 'f1')
+df_1 = pd.concat([df_y, df_x, df_xy], axis=1).round(4)
+df_1.columns = pd.MultiIndex.from_product([['Electricity'], ['Y', 'X', 'XY']])
 
-df.rank(axis=1).mean()
-df.rank(ascending=False).mean(axis=1).sort_values()
+df_y = read_real('Covtype', 'ABRUPT@Y', 'HoeffdingTree', 'f1')
+df_x = read_real('Covtype', 'ABRUPT@X', 'HoeffdingTree', 'f1')
+df_xy = read_real('Covtype', 'ABRUPT@X', 'HoeffdingTree', 'f1')
+df_2 = pd.concat([df_y, df_x, df_xy], axis=1).round(4)
+df_2.columns = pd.MultiIndex.from_product([['Covtype'], ['Y', 'X', 'XY']])
+
+
+df = pd.concat([df_1,df_2],axis=1)
+
+
+def to_latex_tab(df, minimize: bool = False, rotate_cols: bool = False):
+    if rotate_cols:
+        df.columns = [f'\\rotatebox{{90}}{{{x}}}' for x in df.columns]
+
+    annotated_res = []
+    for i, r in df.iterrows():
+        top_2 = r.sort_values(ascending=minimize).unique()[:2]
+        if len(top_2) < 2:
+            raise ValueError('only one score')
+
+        best1 = r[r == top_2[0]].values[0]
+        best2 = r[r == top_2[1]].values[0]
+
+        r[r == top_2[0]] = f'\\textbf{{{best1}}}'
+        r[r == top_2[1]] = f'\\underline{{{best2}}}'
+
+        annotated_res.append(r)
+
+    annotated_res = pd.DataFrame(annotated_res).astype(str)
+
+    # text_tab = annotated_res.to_latex(caption='CAPTION', label='tab:scores_by_ds')
+
+    return annotated_res
+
+
+df = to_latex_tab(df.T, minimize=False).T
+
+
+# Generate LaTeX table
+latex_table = df.to_latex(
+    float_format="%.3f",
+    bold_rows=True,
+    multicolumn=True,
+    multicolumn_format='c',
+    caption='Performance metrics of drift detectors across different datasets',
+    label='tab:combined_metrics',
+    position='htbp'
+)
+
+print(latex_table)
--- scripts/experiments/analysis/detector_evaluation_synth.py
+++ scripts/experiments/analysis/detector_evaluation_synth.py
@@ -1,11 +1,81 @@
 import pandas as pd
 
-results = {}
-for stream in ['Agrawal', 'STAGGER', 'SEA']:
-    results[stream] = pd.read_csv(f'assets/results/{stream},ABRUPT,ARF,POINT.csv').set_index('Unnamed: 0')['f1']
 
-df = pd.DataFrame(results)
-print(df.mean(axis=1).sort_values())
+def read_synth(metric: str, round_to=None):
+    STREAMS = ['Agrawal', 'STAGGER', 'SEA']
+    results = {}
+    for stream in STREAMS:
+        df_ = pd.read_csv(f'assets/results/{stream},ABRUPT,ARF,POINT.csv').set_index('Unnamed: 0')
+        # print(df_.columns.tolist())
+        df_.index.name = 'Detector'
 
-df.rank(axis=1).mean()
-df.rank(ascending=False).mean(axis=1).sort_values()
+        results[stream] = df_[metric]
+
+    df = pd.DataFrame(results)
+
+    if round_to is not None:
+        df = df.round(round_to)
+
+    return df
+
+
+def to_latex_tab(df, minimize: bool = False, rotate_cols: bool = False):
+    if rotate_cols:
+        df.columns = [f'\\rotatebox{{90}}{{{x}}}' for x in df.columns]
+
+    annotated_res = []
+    for i, r in df.iterrows():
+        top_2 = r.sort_values(ascending=minimize).unique()[:2]
+        if len(top_2) < 2:
+            raise ValueError('only one score')
+
+        best1 = r[r == top_2[0]].values[0]
+        best2 = r[r == top_2[1]].values[0]
+
+        r[r == top_2[0]] = f'\\textbf{{{best1}}}'
+        r[r == top_2[1]] = f'\\underline{{{best2}}}'
+
+        annotated_res.append(r)
+
+    annotated_res = pd.DataFrame(annotated_res).astype(str)
+
+    # text_tab = annotated_res.to_latex(caption='CAPTION', label='tab:scores_by_ds')
+
+    return annotated_res
+
+
+df_f1 = read_synth('f1', round_to=4)
+df_f1 = to_latex_tab(df_f1.T, minimize=False).T
+df_er = read_synth('ep_recall', round_to=4)
+df_er = to_latex_tab(df_er.T, minimize=False).T
+df_far = read_synth('fa_1k', round_to=4)
+df_far = to_latex_tab(df_far.T, minimize=True).T
+
+# print(df.mean(axis=1).sort_values())
+# df.rank(axis=1).mean()
+# df.rank(ascending=False).mean(axis=1).sort_values()
+
+
+combined_df = pd.DataFrame(index=df_f1.index)
+
+for dataset in ['Agrawal', 'STAGGER', 'SEA']:
+    combined_df[(dataset, 'F1')] = df_f1[dataset]
+    combined_df[(dataset, 'Recall')] = df_er[dataset]
+    combined_df[(dataset, 'FAR')] = df_far[dataset]
+
+# Create proper MultiIndex for columns
+combined_df.columns = pd.MultiIndex.from_product([['Agrawal', 'STAGGER', 'SEA'], ['F1', 'Recall', 'FAR']])
+combined_df.index.name = 'Detector'
+
+# Generate LaTeX table
+latex_table = combined_df.to_latex(
+    float_format="%.3f",
+    bold_rows=True,
+    multicolumn=True,
+    multicolumn_format='c',
+    caption='Performance metrics of drift detectors across different datasets',
+    label='tab:combined_metrics',
+    position='htbp'
+)
+
+print(latex_table)
