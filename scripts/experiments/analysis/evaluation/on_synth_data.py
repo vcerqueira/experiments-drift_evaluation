fa742e4b31cbfe38cc89405f1f2e918439ea7353
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-05-13 13:38:32 +0100

    synth analysis

--- scripts/experiments/analysis/evaluation/on_synth_data.py
+++ scripts/experiments/analysis/evaluation/on_synth_data.py
@@ -2,10 +2,17 @@ import pandas as pd
 
 from src.misc import prep_latex_tab, DataReader
 
+
+MODE = 'ABRUPT'
+
 df_f1 = DataReader.get_synth_results(metric='f1', round_to=3)
 df_f1 = df_f1.drop(columns=['Classifier'])
-df_f1_tab = prep_latex_tab(df_f1.set_index(['Stream', 'Mode']),
-                           rotate_cols=True,
+df_f1 = df_f1.query(f'Mode=="{MODE}"').drop(columns=['Mode'])
+# df_f1 = df_f1.set_index(['Stream', 'Mode'])
+df_f1 = df_f1.set_index(['Stream'])
+
+df_f1_tab = prep_latex_tab(df_f1,
+                           rotate_cols=False,
                            minimize=False)
 
 # df_er = DataReader.get_synth_results(metric='episode_recall', round_to=4)
@@ -15,21 +22,24 @@ df_f1_tab = prep_latex_tab(df_f1.set_index(['Stream', 'Mode']),
 
 df_far = DataReader.get_synth_results(metric='far', round_to=3)
 df_far = df_far.drop(columns=['Classifier'])
-df_far_tab = prep_latex_tab(df_far.set_index(['Stream', 'Mode']),
-                            rotate_cols=True,
+df_far = df_far.query(f'Mode=="{MODE}"').drop(columns=['Mode'])
+# df_far = df_far.set_index(['Stream', 'Mode'])
+df_far = df_far.set_index(['Stream'])
+
+df_far_tab = prep_latex_tab(df_far,
+                            rotate_cols=False,
                             minimize=True)
 
-combined_df = pd.concat({'F1': df_f1_tab,
-                         # 'ER': df_er_tab,
-                         'FAR': df_far_tab})
+combined_df = pd.concat({'F1': df_f1_tab, 'FAR': df_far_tab})
+combined_df = combined_df.swaplevel(0, 1).sort_index()
 
-# Generate LaTeX table
-latex_table = combined_df.to_latex(
+
+latex_table = combined_df.T.to_latex(
     float_format="%.3f",
     bold_rows=True,
     multicolumn=True,
     multicolumn_format='c',
-    caption='Performance metrics of drift detectors across different datasets',
+    caption=f'Performance metrics of drift detectors across different datasets {MODE}',
     label='tab:combined_metrics',
     position='htbp'
 )
